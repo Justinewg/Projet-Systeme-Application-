7f16defaf57be8664ad617db762b4f90a1d9a2b8
HaroldMolter <harold.molter@efrei.net>
2022-09-19 17:34:34 +0200

    Pizza classes with design pattern
    Implementation of pizza classes with a decorator patttern

---
+++ .vs/Projet-Systeme-Application-/PizzaDecoratorPattern/PizzaDecoratorPattern/Pizza.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public interface Pizza
+{
+    string MakePizza();
+}
+
+public class PlainPizza : Pizza
+{
+    public string MakePizza()
+    {
+        return "Plain Pizza";
+    }
+}
+
+public abstract class PizzaDecorator : Pizza
+{
+    protected Pizza pizza;
+
+    public PizzaDecorator(Pizza pizza)
+    {
+        this.pizza = pizza;
+    }
+
+    public virtual string MakePizza()
+    {
+        return pizza.MakePizza();
+    }
+}
+
+
+public class CheeseDecorator : PizzaDecorator
+{
+    public CheeseDecorator(Pizza pizza) : base(pizza) { }
+
+    public override string MakePizza()
+    {
+        return pizza.MakePizza() + addCheese();
+    }
+
+    private string addCheese()
+    {
+        return ", Cheese added";
+    }
+}
+
+public class TomatoDecorator : PizzaDecorator
+{
+    public TomatoDecorator(Pizza pizza) : base(pizza) { }
+
+    public override string MakePizza()
+    {
+        return pizza.MakePizza() + addTomato();
+    }
+
+    private string addTomato()
+    {
+        return ", Tomato added";
+    }
+}
+
+public class VeggieDecorator : PizzaDecorator
+{
+    public VeggieDecorator(Pizza pizza) : base(pizza) { }
+
+    public override string MakePizza()
+    {
+        return pizza.MakePizza() + addVeggie();
+    }
+
+    private string addVeggie()
+    {
+        return ", Vegetables added";
+    }
+}
---
+++ .vs/Projet-Systeme-Application-/PizzaDecoratorPattern/PizzaDecoratorPattern/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        PlainPizza plainPizzaObj = new PlainPizza();
+        string plainPizza = plainPizzaObj.MakePizza();
+        Console.WriteLine(plainPizza);
+
+        PizzaDecorator cheeseDecorator = new CheeseDecorator(plainPizzaObj);
+        string cheesePizza = cheeseDecorator.MakePizza();
+        Console.WriteLine("\n'" + cheesePizza + "' using CheeseDecorator");
+
+        VeggieDecorator veggieDecorator = new VeggieDecorator(plainPizzaObj);
+        string veggiePizza = veggieDecorator.MakePizza();
+
+        TomatoDecorator tomatoDecorator = new TomatoDecorator(plainPizzaObj);
+        veggiePizza = tomatoDecorator.MakePizza();
+        Console.WriteLine("\n'" + veggiePizza + "' using VeggieDecorator");
+
+        Console.Read();
+    }
+}
