4c818f1c65cd17389c43c835463125dc6b9b1644
RomaneSimonet99 <73843327+RomaneSimonet99@users.noreply.github.com>
2022-09-27 13:52:46 +0200

    fonction appel bougé dans le main

--- Client.cs
+++ Client.cs
@@ -22,35 +22,7 @@ namespace Projet
         }
 
 
-        public static boolean appel()
-        {
-            Console.WriteLine("Est-ce votre première commande? O / N");
-            string reponse = Console.ReadLine();
-            while (reponse != 'O' ^ reponse != 'N')
-            {
-                if ((reponse = Console.ReadLine()) = 'O')
-                {
-                    Console.WriteLine('Entrez votre nom de famille.');
-                    string nom = Console.ReadLine();
-                    Console.WriteLine('Entrez votre prénom.');
-                    string prenom = Console.ReadLine();
-                    Console.WriteLine('Entrez votre adresse.');
-                    string adr = Console.ReadLine();
-                    Console.WriteLine('Entrez votre numéro de téléphone.');
-                    int num_tel = Console.ReadLine();
-                    Console.WriteLine('Entrez la date de votre première commande.');
-                    DateTime date_pr_com = Console.ReadLine();
-
-                    Client nv_client = new Client(nom,prenom, num_tel, adr, date_pr_com, 0);
-
-                }
-                else if ((reponse = Console.ReadLine()) = 'N')
-                {
-                    Console.WriteLine('Pouvez entrer votre numéro de téléphone svp.');
-                    int num_tel = Console.ReadLine();
-                }
-            }
-        }
+       
 
         public static void nouveau_client(nom, prenom, adr, num_tel, date_pr_com)
         {
--- Program.cs
+++ Program.cs
@@ -7,6 +7,36 @@ namespace Projet
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            public static boolean appel()
+            {
+                Console.WriteLine("Est-ce votre première commande? O / N");
+                string reponse = Console.ReadLine();
+                while (reponse != 'O' ^ reponse != 'N')
+                {
+                    if ((reponse = Console.ReadLine()) = 'O')
+                    {
+                        Console.WriteLine('Entrez votre nom de famille.');
+                        string nom = Console.ReadLine();
+                        Console.WriteLine('Entrez votre prénom.');
+                        string prenom = Console.ReadLine();
+                        Console.WriteLine('Entrez votre adresse.');
+                        string adr = Console.ReadLine();
+                        Console.WriteLine('Entrez votre numéro de téléphone.');
+                        int num_tel = Console.ReadLine();
+                        Console.WriteLine('Entrez la date de votre première commande.');
+                        DateTime date_pr_com = Console.ReadLine();
+
+                        Client nv_client = new Client(nom, prenom, num_tel, adr, date_pr_com, 0);
+
+                    }
+                    else if ((reponse = Console.ReadLine()) = 'N')
+                    {
+                        Console.WriteLine('Pouvez entrer votre numéro de téléphone svp.');
+                        int num_tel = Console.ReadLine();
+                    }
+                }
+            }
         }
     }
 }
